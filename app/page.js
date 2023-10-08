33964ab33eeef2f0e18fe980842453f0029c7f5b
Santhosh <angadipetasanthu@gmail.com>
2023-10-08 16:31:54 +0530

    added Dishlist

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
     flex-col items-center justify-between ">
       
       <div>
-        {/* <DishList/> */}
+        <DishList/>
         
       </div>
       <Footer/>
